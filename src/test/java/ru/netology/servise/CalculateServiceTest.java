b179140aefe52e83a3474517b9dab065fe3f875b
sedrik13 <122992058+sedrik13@users.noreply.github.com>
2023-09-08 11:10:11 +0300

    Update and rename calculateserviceTest.java to CalculateServiceTest.java
    add_12

--- src/test/java/ru/netology/servise/CalculateServiceTest.java
+++ src/test/java/ru/netology/servise/CalculateServiceTest.java
@@ -7,25 +7,25 @@ import ru.netology.services.CalculateService;
 
 public class CalculateServiceTest {
     @Test
-    public void test() {
+    public void testMonth_3() {
         CalculateService service = new CalculateService();
 
         int income = 10_000; //ежемесячный доход
         int expenses = 3_000; //ежемесячные траты
         int threshold = 20_000; // сумма при которой можно отдохнуть
-        int count = service.calculate(income, expenses, threshold);
+        int count = service.calculateMonth(income, expenses, threshold);
         //System.out.println("количество месяцев отдыха равно " + count);
         Assertions.assertEquals(3, count);
+    }
+        @Test
+        public void testMonth_2() {
+        CalculateService service = new CalculateService();
 
-        //@Test
-        //public void test() {
-        //CalculateService service = new CalculateService();
-
-        //int income = 100_000; //ежемесячный доход
-        //int expenses = 60_000; //ежемесячные траты
-        //int threshold = 150_000; // сумма при которой можно отдохнуть
-        //int count = service.calculate(income, expenses, threshold);
+        int income = 100_000; //ежемесячный доход
+        int expenses = 60_000; //ежемесячные траты
+        int threshold = 150_000; // сумма при которой можно отдохнуть
+        int count = service.calculateMonth(income, expenses, threshold);
         ////System.out.println("количество месяцев отдыха равно " + count);
-        //Assertions.assertEquals(2, count);
+        Assertions.assertEquals(2, count);
     }
 }
